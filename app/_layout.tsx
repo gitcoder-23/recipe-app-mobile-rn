1d6ebfa0d6aa5018421f21a917520ba6ec254bf2
gitcoder-23 <prabhatece21@gmail.com>
2025-11-08 17:31:43 +0530

    app done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Slot } from "expo-router";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import SafeScreen from "@/components/SafeScreen";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Slot />
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
       {/* <SafeAreaView style={{ flex: 1 }}>
         <Stack screenOptions={{ headerShown: false }} />
         <StatusBar style="auto" />
